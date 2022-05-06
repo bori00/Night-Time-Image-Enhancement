37252fc62c407621ef9c32ac11ee33bafb2c3fde
bori00 <fazakasbori@gmail.com>
2022-05-06 19:41:09 +0300

    save generated images in a specified folder, as bmp files

--- nighttime_enhancement_main.cpp
+++ nighttime_enhancement_main.cpp
@@ -1,9 +1,5 @@
-// OpenCVApplication.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include "common.h"
-
 #include "PatchOrderedFilter.h"
 #include "AtmosphericLightEstimator.h"
 #include "TransmissionEstimator.h"
@@ -15,123 +11,75 @@
 // patch size = (2 * PATCH_RADIUS + 1) x (2 * PATC_RADIUS + 1)
 // the higher the patch size, the longer the computation time, 
 // but the brighter and clearer the final image
-const int PATCH_RADIUS = 5;
-
-const bool COMPUTE_BRUTE_FORCE_CHANNELS = 0;
-
-const double ALPHA = 0.4; // for dark-channel-based transmission estimate correction
-const double EPSYLON = 1e3; // for the guided filtering
+const int PATCH_RADIUS = 9;
+// for dark-channel-based transmission estimate correction
+const double ALPHA = 0.4;
+// for the guided filtering
+const double EPSYLON = 1e3;
 
 
 int main()
 {
 	char fname[MAX_PATH];
 
-	AtmosphericLightEstimator atm_light_estimator(0.1);
-
-	ChannelIntensityMaxFilter max_filter;
-
-	ChannelIntensityMinFilter min_filter;
-
 	while (openFileDlg(fname))
 	{
+		// --------------- 0th step. Input
+		Mat img = imread(fname, IMREAD_COLOR);
 		printf("----------New Image: %s \n", fname);
 
-		Mat img = imread(fname, IMREAD_COLOR);
-
-		double t = (double)getTickCount(); // Get the current time [s]
-
-		Mat bright_channel_img = DynamicPatchOrderedFilter().getFilteredImg(img, PATCH_RADIUS, &max_filter);
-
-		Mat dark_channel_img = DynamicPatchOrderedFilter().getFilteredImg(img, PATCH_RADIUS, &min_filter);
-
-		// Get the current time again and compute the time difference [s]
-		t = ((double)getTickCount() - t) / getTickFrequency();
-		// Print (in the console window) the processing time in [ms] 
-		printf("Time (Dynamic) = %.3f [ms]\n", t * 1000);
-
-		// todo: remove entire if. Used for testing only
-		if (COMPUTE_BRUTE_FORCE_CHANNELS) {
-
-			t = (double)getTickCount(); // Get the current time [s]
-
-			Mat brute_bright_channel_img = BruteForcePatchOrderedFilter().getFilteredImg(img, PATCH_RADIUS, &max_filter);
-
-			Mat brute_dark_channel_img = BruteForcePatchOrderedFilter().getFilteredImg(img, PATCH_RADIUS, &min_filter);
-
-			// Get the current time again and compute the time difference [s]
-			t = ((double)getTickCount() - t) / getTickFrequency();
-			// Print (in the console window) the processing time in [ms] 
-			printf("Time (Brute Force) = %.3f [ms]\n", t * 1000);
-
-			bool eq_bright = cv::countNonZero(bright_channel_img != brute_bright_channel_img) == 0;
-
-			bool eq_dark = cv::countNonZero(dark_channel_img != brute_dark_channel_img) == 0;
-
-			printf("The equality of bright=%d and dark=%d\n", eq_bright, eq_dark);
-
-			assert(eq_bright && eq_dark);
-		}
+		printf("The name of the output folder:");
+		std::string output_foldername;
+		std::cin >> output_foldername;
 
 		imshow("Original Image", img);
+		imwrite(output_foldername + "\\OriginalImage.bmp", img);
 
+		// --------------- 1st step. Bright- and dark-channel computation.
+		ChannelIntensityMaxFilter max_filter;
+		Mat bright_channel_img = DynamicPatchOrderedFilter().getFilteredImg(img, PATCH_RADIUS, &max_filter);
 		imshow("Bright Channel", bright_channel_img);
+		imwrite(output_foldername + "\\BrightChannel.bmp", bright_channel_img);
 
+		ChannelIntensityMinFilter min_filter;
+		Mat dark_channel_img = DynamicPatchOrderedFilter().getFilteredImg(img, PATCH_RADIUS, &min_filter);
 		imshow("Dark Channel", dark_channel_img);
+		imwrite(output_foldername + "\\DarkChannel.bmp", dark_channel_img);
 
+		// --------------- 2nd step. Atmospheric light estimation
+		AtmosphericLightEstimator atm_light_estimator(0.1);
 		Vec3b atm_light = atm_light_estimator.getEstimate(img, bright_channel_img);
-
-		printf("Global atmospheric light: (%d, %d, %d)\n", atm_light[0], atm_light[1], atm_light[2]);
-
-
-		// 1. enhance using bright channel only
+		
+		// --------------- 3rd step. Estimating the transmission T based on the bright channel only.
 		BrightOnlyTransmissionEstimator t_estimator1;
-
 		Mat t_estimate1 = t_estimator1.getEstimation(bright_channel_img, dark_channel_img, atm_light);
-
 		imshow("bright channel t estimation", t_estimate1);
+		imwrite(output_foldername + "\\BrightChannelTEstimate.bmp", t_estimate1 * 255);
 
 		Mat imgJ1 = ImageReconstructor().reconstruct(img, atm_light, t_estimate1);
-
 		imshow("Bright-Only Enhanced Image", imgJ1);
+		imwrite(output_foldername + "\\BrightChannelEnhancedImage.bmp", imgJ1);
 
-		// 2. enhance using both channels
+		// --------------- 4th step. Estimating the transmission T based on both the bright and the dark channel.
 		DoubleChannelTransmissionEstimator t_estimator2(ALPHA);
-
 		Mat t_estimate2 = t_estimator2.getEstimation(bright_channel_img, dark_channel_img, atm_light);
-
 		imshow("double channel t estimation", t_estimate2);
+		imwrite(output_foldername + "\\DoubleChannelTEstimate.bmp", t_estimate2 * 255);
 
 		Mat imgJ2 = ImageReconstructor().reconstruct(img, atm_light, t_estimate2);
-
 		imshow("Double-Channel Enhanced Image", imgJ2);
+		imwrite(output_foldername + "\\DoubleChannelEnhancedImage.bmp", imgJ2);
 
-		// 3. enhance using both channels and guided filtering
+		// --------------- 5th step. Refining the transmission map using a guided filter.
 		GuidedFilteringDoubleChannelTransmissionEstimator t_estimator3(ALPHA, EPSYLON, PATCH_RADIUS * 2 + 1, img);
-
 		Mat t_estimate3 = t_estimator3.getEstimation(bright_channel_img, dark_channel_img, atm_light);
-
 		imshow("guided filtering double channel t estimation", t_estimate3);
+		imwrite(output_foldername + "\\GuidedDoubleChannelTEstimate.bmp", t_estimate3 * 255);
 
+		// --------------- 6th step. Obtaining the well-exposed image based on the refined transmission estimation.
 		Mat imgJ3 = ImageReconstructor().reconstruct(img, atm_light, t_estimate3);
-
 		imshow("Guided Double-Channel Enhanced Image", imgJ3);
-
-		// comparison
-
-		/*Mat imgComp;
-		cv::absdiff(imgJ1, imgJ2, imgComp);
-
-		imshow("Difference", imgComp);*/
-
-		// simply normalized image
-
-		/*Mat imgNorm;
-		cv::normalize(img, imgNorm, 0, 255, cv::NORM_MINMAX);
-
-		imshow("Simply Normalized Image", imgNorm);
-
-		waitKey();*/
+		imwrite(output_foldername + "\\GuidedDoubleChannelEnhancedImage.bmp", imgJ3);
 	}
 	return 0;
 }
